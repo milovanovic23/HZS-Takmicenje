1640f8f112b0eb4d50e54e3aa3e5bc600e2d8831
Desamax1 <despotmaksimovic@gmail.com>
2022-12-10 23:37:56 +0100

    added nav active detection

--- client/src/components/Layout/Navbar/Navbar.jsx
+++ client/src/components/Layout/Navbar/Navbar.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
-  return (
-    <nav>
-        <ul>
-            <li>
-                <Link className='link' to="/">About</Link>
-            </li>
-            <li>
-                &nbsp;/
-            </li>
-            <li>
-                <Link className='link' to="/stories">Stories</Link>
-            </li>
-            <li>
-                &nbsp;/
-            </li>
-            <li>
-                <Link className='link' to="/game">Test yourself</Link>
-            </li>
-            <li>
-                &nbsp;/
-            </li>
-            <li>
-                <Link className='link' to="/contact-us">Contact us</Link>
-            </li>
-        </ul>
-    </nav>
-  );
+    const location = useLocation();
+    const { pathname } = location;
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link className='link' to="/">
+                        { pathname === "/" ? <div className="active">About</div> : "About"}
+                    </Link>
+                </li>
+                <li>
+                    &nbsp;/
+                </li>
+                <li>
+                    <Link className='link' to="/stories">Stories</Link>
+                </li>
+                <li>
+                    &nbsp;/
+                </li>
+                <li>
+                    <Link className='link' to="/game">Test yourself</Link>
+                </li>
+                <li>
+                    &nbsp;/
+                </li>
+                <li>
+                    <Link className='link' to="/contact-us">Contact us</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
