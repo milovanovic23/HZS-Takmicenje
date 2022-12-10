ff3438212489bd4b52c618384748e5f0e08f1667
milovanovic23 <milovanovic8filip@gmail.com>
2022-12-10 21:44:32 +0100

    filip3

--- client/src/Pages/Stories/Stories.jsx
+++ client/src/Pages/Stories/Stories.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Post from '../../components/Post/Post.jsx'
-
+import './Stories.css'
 const Stories = () => {
   return (
     <div>
-      <h1>anonymous stories</h1>
+      <h1 className='anonStories'>anonymous stories</h1>
       <Post title='Lorem ipsum' bodyText='Lorem ipsum dolor'/>
+      <Post title='Lorem ipsum' bodyText='Lorem ipsum dolor'/>
+      <Post title='Lorem ipsum' bodyText='Lorem ipsum dolor'/>
+      <Post title='Lorem ipsum' bodyText='Lorem ipsum dolor'/>
+    
+    
+    
     </div>
   );
 }
