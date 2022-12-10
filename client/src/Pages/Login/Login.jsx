b0462aeb1057e16e168d2be3ad25dbc923313f4f
Desamax1 <despotmaksimovic@gmail.com>
2022-12-11 00:08:25 +0100

    added login page

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -5,7 +5,15 @@ const Login = () => {
   return (
     <div className="auth">
       <div className="auth-wrapper">
-        <h2></h2>
+        <div className="header">
+          <h2>/ Login /</h2>
+          <h3>First time with us? Create an account</h3>
+        </div>
+        <div className="inputs">
+          <input type="text" name="email" id="email" placeholder='E-Mail' />
+          <input type="password" name="password" id="password" placeholder='Password' />
+        </div>
+        <input type="submit" value="Log in" />
       </div>
     </div>
   );
