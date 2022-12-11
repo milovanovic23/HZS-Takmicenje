88f39f1bc188d6aca7810aae7025ee2f9e632113
milovanovic23 <milovanovic8filip@gmail.com>
2022-12-11 05:24:13 +0100

    filip11

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -7,20 +7,24 @@ import img2 from '../../assets/landing2.png';
 const Home = () => {
     return (
         <div className="landing">
+            
             <h1>
                 The trick <br />
                 is to <br />
                 communicate
             </h1>
+
+        
+           
             <section>
-                <div>
+                <div className='sectionLandingText'>
                     <p>The internet is involved in everyone’s life today, but as much good as it brings, it brings the same amount of bad things that could happen to you if you stumble upon the bad part of the internet. Our story-based, point-and-click games try to help everyone understand the importance of trying to find help for yourself or someone that needs it.</p>
                 </div>
                 <img src={img1} alt="IMG1" />
             </section>
-            <section>
+            <section >
                 <img src={img2} alt="IMG2" />
-                <div>
+                <div className='sectionLandingText'>
                     <p>Also, a big part of our goal is to bring awareness to what could happen in the worst possible scenario, and help people find good in themselves and the others.</p>
                 </div>
             </section>
