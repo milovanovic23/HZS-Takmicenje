19f889a426f8c18bde2852002477fb2037bb1fc0
milovanovic23 <milovanovic8filip@gmail.com>
2022-12-11 01:35:17 +0100

    filip7

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './Login.css';
+import banner from "../../assets/banner1.png";
 
 const Login = () => {
   return (
     <div className="auth">
       <div className="auth-wrapper">
+        <img src={banner} alt="text" />
+        <div className="authContent">
         <div className="header">
-          <h2>/ Login /</h2>
-          <h3>First time with us? Create an account</h3>
+          <h2>Login</h2>
+          <h3 className='registerLink'>First time with us? Create an account.</h3>
         </div>
         <div className="inputs">
           <input type="text" name="email" id="email" placeholder='E-Mail' />
           <input type="password" name="password" id="password" placeholder='Password' />
         </div>
         <input type="submit" value="Log in" />
+        </div>
+        
       </div>
     </div>
   );
